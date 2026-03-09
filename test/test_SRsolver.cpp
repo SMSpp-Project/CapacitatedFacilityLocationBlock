d65a28f47e01e448e6abb5f8cb1d46572694093b
Donato Meoli <donato.meoli.95@gmail.com>
2026-03-09 08:51:22 -0500

    spaces

--- test/test_SRsolver.cpp
+++ test/test_SRsolver.cpp
@@ -70,7 +70,6 @@ using namespace SMSpp_di_unipi_it;
 /// Custom terminate function to print the exception message
 
 void smspp_terminate( void ) {
-
  std::cerr << "Uncaught exception in executing SMS++:\n";
  try {
   std::rethrow_exception( std::current_exception() );
--- tools/txt2nc4.cpp
+++ tools/txt2nc4.cpp
@@ -40,17 +40,17 @@ using namespace SMSpp_di_unipi_it;
 /// Custom terminate function to print the exception message
 
 void smspp_terminate( void ) {
-    std::cerr << "Uncaught exception in executing SMS++:\n";
-    try {
-        std::rethrow_exception( std::current_exception() );
-    }
-    catch( const std::exception & e ) {
-        std::cerr << "\tException type: " << typeid( e ).name() << "\n";
-        std::cerr << "\tException message: " << e.what() << "\n";
-    } catch( ... ) {
-        std::cerr << "\tUnknown exception" << std::endl;
-    }
-    std::abort(); // or exit(1)
+ std::cerr << "Uncaught exception in executing SMS++:\n";
+ try {
+  std::rethrow_exception( std::current_exception() );
+ }
+ catch( const std::exception & e ) {
+  std::cerr << "\tException type: " << typeid( e ).name() << "\n";
+  std::cerr << "\tException message: " << e.what() << "\n";
+ } catch( ... ) {
+  std::cerr << "\tUnknown exception" << std::endl;
+ }
+ std::abort(); // or exit(1)
 }
 
 /*--------------------------------------------------------------------------*/
